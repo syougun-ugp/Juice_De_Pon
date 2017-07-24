927f0e70b980380929f4466ea3fa7e878f21419f
syougun-ugp <syougun360.main@gmail.com>
2017-07-24 22:42:03 +0900

    WebGL対応

--- Assets/Script/Director.cs
+++ Assets/Script/Director.cs
@@ -10,6 +10,8 @@ public class Director : MonoBehaviour {
 	void Start () {
         DontDestroyOnLoad(gameObject);
 
+		Debug.logger.logEnabled = false;
+
 //        var eval = "window.open('" + publicityURL + "','宣伝', 'width=800, height=600, menubar=no,location=no,status=no, toolbar=no, scrollbars=yes')";
 //        Application.ExternalEval(eval);
 	}
